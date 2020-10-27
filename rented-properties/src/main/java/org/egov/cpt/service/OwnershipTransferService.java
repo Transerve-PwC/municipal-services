eb52166d4011dddbf2ca647c2335ed60047dfc88
Transerveshivam <shivam.gaba@transerve.com>
2020-10-27 16:37:32 +0530

    number validation added for property and ownership transfer

--- rented-properties/src/main/java/org/egov/cpt/service/OwnershipTransferService.java
+++ rented-properties/src/main/java/org/egov/cpt/service/OwnershipTransferService.java
@@ -71,6 +71,7 @@ public class OwnershipTransferService {
 
 	public List<Owner> createOwnershipTransfer(OwnershipTransferRequest request) {
 		// propertyValidator.validateCreateRequest(request);
+		propertyValidator.validateNumericValueForOwnershipTransfer(request);
 		List<Property> propertyFromSearch = propertyValidator.getPropertyForOT(request);
 		enrichmentService.enrichCreateOwnershipTransfer(request, propertyFromSearch);
 		if (config.getIsWorkflowEnabled()) {
@@ -125,6 +126,7 @@ public class OwnershipTransferService {
 	}
 
 	public List<Owner> updateOwnershipTransfer(OwnershipTransferRequest request) {
+		propertyValidator.validateNumericValueForOwnershipTransfer(request);
 		List<Owner> ownersFromSearch = propertyValidator.validateUpdateRequest(request);
 		enrichmentService.enrichUpdateOwnershipTransfer(request, ownersFromSearch);
 		String applicationState = request.getOwners().get(0).getApplicationState(); // demand generation
