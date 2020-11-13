5a6d9a264e7e095af69e0d7c4092b4066a6cbb9c
Transerveshivam <shivam.gaba@transerve.com>
2020-11-13 18:48:23 +0530

    offline payment details query updated

--- rented-properties/src/main/resources/db/migration/main/V20201015120001__cs_pt_offline_payment_detail_ddl.sql
+++ rented-properties/src/main/resources/db/migration/main/V20201015120001__cs_pt_offline_payment_detail_ddl.sql
@@ -10,6 +10,11 @@ CREATE TABLE cs_pt_offline_payment_detail (
    bankname                     CHARACTER VARYING (100),
    transactionnumber	        CHARACTER VARYING (100),
 
+   created_by           CHARACTER VARYING (128) NOT NULL,
+   created_date         CHARACTER VARYING NOT NULL,
+   modified_by     		CHARACTER VARYING (128),
+   modified_date       	CHARACTER VARYING,
+
   CONSTRAINT pk_cs_pt_offline_payment_detail PRIMARY KEY (id),
   CONSTRAINT fk_cs_pt_offline_payment_detail FOREIGN KEY (property_id) REFERENCES cs_pt_property_v1 (id)
   ON UPDATE CASCADE
