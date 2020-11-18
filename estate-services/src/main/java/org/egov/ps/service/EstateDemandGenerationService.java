f312098a3aeb96259c493105eabb5836540da4c7
pooja-transerve <pooja.p@transerve.com>
2020-11-18 15:45:29 +0530

    Completed test case for generate estate demand

--- estate-services/src/main/java/org/egov/ps/service/EstateDemandGenerationService.java
+++ estate-services/src/main/java/org/egov/ps/service/EstateDemandGenerationService.java
@@ -1,10 +1,7 @@
 package org.egov.ps.service;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.Instant;
 import java.time.LocalDate;
-import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.Calendar;
@@ -32,9 +29,13 @@ import org.egov.ps.web.contracts.EstateAccount;
 import org.egov.ps.web.contracts.EstateDemand;
 import org.egov.ps.web.contracts.EstatePayment;
 import org.egov.ps.web.contracts.PropertyRequest;
+import org.joda.time.DateTimeComparator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -61,8 +62,8 @@ public class EstateDemandGenerationService {
 	public AtomicInteger createDemand(EstateDemandCriteria demandCriteria) {
 		AtomicInteger counter = new AtomicInteger(0);
 		PropertyCriteria propertyCriteria = new PropertyCriteria();
-		//propertyCriteria.setRelations(Collections.singletonList("owner"));
-		//propertyCriteria.setState(Arrays.asList(PSConstants.PM_APPROVED));
+		propertyCriteria.setRelations(Collections.singletonList("owner"));
+		propertyCriteria.setState(Arrays.asList(PSConstants.PM_APPROVED));
 		List<Property> propertyList = propertyRepository.getProperties(propertyCriteria);
 		
 		propertyList.forEach(property -> {
@@ -89,7 +90,17 @@ public class EstateDemandGenerationService {
 					List<Long> dateList = estateDemandList.stream().map(r -> r.getGenerationDate())
 							.collect(Collectors.toList());
 					Date date = demandCriteria.isEmpty() ? new Date() : FORMATTER.parse(demandCriteria.getDate());
-					if (!isMonthIncluded(dateList, date)) {
+					Date generateDemandDate = getFirstDateOfMonth(new Date());
+					
+					if(!CollectionUtils.isEmpty(property.getPropertyDetails().getPaymentConfigs())) {
+						PaymentConfig paymentConfig = property.getPropertyDetails().getPaymentConfigs().get(0);
+						if(paymentConfig.getIsGroundRent() && paymentConfig.getGroundRentGenerationType().equalsIgnoreCase(PSConstants.MONTHLY)) {
+							generateDemandDate = setDateOfMonth(date,Integer.parseInt(paymentConfig.getGroundRentGenerateDemand()));
+						}
+					}
+					DateTimeComparator.getDateOnlyInstance().compare(date, generateDemandDate);
+					if (!isMonthIncluded(dateList, date) && 
+							DateTimeComparator.getDateOnlyInstance().compare(date, generateDemandDate) == 0) {
 						// generate demand
 						counter.getAndIncrement();
 						generateEstateDemand(property, firstDemand.get(), getFirstDateOfMonth(date), estateDemandList,
@@ -112,15 +123,13 @@ public class EstateDemandGenerationService {
 			List<EstateDemand> estateDemandList, List<EstatePayment> estatePaymentList, EstateAccount estateAccount) {		
 		
 		Double collectionPrincipal = firstDemand.getCollectionPrincipal();
-		
+		Double calculatedRent = calculateRentAccordingtoMonth(property, date);
 		if(!CollectionUtils.isEmpty(property.getPropertyDetails().getPaymentConfigs())) {
 			PaymentConfig paymentConfig = property.getPropertyDetails().getPaymentConfigs().get(0);
 			if(paymentConfig.getIsGroundRent() && paymentConfig.getGroundRentGenerationType().equalsIgnoreCase(PSConstants.MONTHLY)) {
 				date = setDateOfMonth(date,Integer.parseInt(paymentConfig.getGroundRentGenerateDemand()));
 			}
 		}
-		
-		Double calculatedRent = calculateRentAccordingtoMonth(property.getPropertyDetails().getPaymentConfigs(), date);
 		
 		AuditDetails auditDetails = AuditDetails.builder().createdBy("System").createdTime(new Date().getTime())
 				.lastModifiedBy("System").lastModifiedTime(new Date().getTime()).build();
@@ -129,7 +138,6 @@ public class EstateDemandGenerationService {
 				/*.mode(ModeEnum.GENERATED)*/.generationDate(date.getTime()).collectionPrincipal(collectionPrincipal)
 				.auditDetails(auditDetails).remainingPrincipal(collectionPrincipal).interestSince(date.getTime())
 				.rent(calculatedRent).build();
-		
 		property.getPropertyDetails().getEstateDemands().add(estateDemand);
 		
 		log.info("Generating Estate demand id '{}' of principal '{}' for property with file no {}", estateDemand.getId(),
@@ -157,7 +165,9 @@ public class EstateDemandGenerationService {
 		producer.push(config.getUpdatePropertyTopic(), propertyRequest);
 	}
 		
-	private Double calculateRentAccordingtoMonth(List<PaymentConfig> paymentConfigs, Date requestedDate) {
+	private Double calculateRentAccordingtoMonth(Property property, Date requestedDate) {
+		List<PaymentConfig> paymentConfigs = property.getPropertyDetails().getPaymentConfigs();
+		AtomicInteger checkLoopIf = new AtomicInteger();
 		if(!CollectionUtils.isEmpty(paymentConfigs)) {
 			for(PaymentConfig paymentConfig : paymentConfigs) {
 				Date startDate = new Date(paymentConfig.getGroundRentBillStartDate());
@@ -170,12 +180,17 @@ public class EstateDemandGenerationService {
 				        LocalDate.parse(endDateText).withDayOfMonth(1));
 				
 				for(PaymentConfigItems paymentConfigItem : paymentConfig.getPaymentConfigItems()) {
-					if(paymentConfigItem.getGroundRentStartMonth() >= monthsBetween 
+					if(paymentConfigItem.getGroundRentStartMonth() <= monthsBetween 
 							&& monthsBetween <= paymentConfigItem.getGroundRentEndMonth()) {
+						checkLoopIf.incrementAndGet();
 						return paymentConfigItem.getGroundRentAmount().doubleValue();
 					}
 				}
 			}
+		}
+		if(checkLoopIf.get() == 0) {
+			log.error("exception occured becasue of no paymentconfig is avaliable provided date:"+new SimpleDateFormat("yyyy-MM-dd").format(requestedDate)
+					+" for property id: " + property.getId());
 		}
 		return 0.0;
 	}
--- estate-services/src/test/java/org/egov/ps/service/EstateDemandGenerationServiceTests.java
+++ estate-services/src/test/java/org/egov/ps/service/EstateDemandGenerationServiceTests.java
@@ -1,12 +1,19 @@
 package org.egov.ps.service;
 
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyList;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Mockito.when;
+
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.egov.ps.config.Configuration;
+import org.egov.ps.model.EstateDemandCriteria;
 import org.egov.ps.model.ModeEnum;
 import org.egov.ps.model.PaymentConfig;
 import org.egov.ps.model.PaymentConfigItems;
@@ -15,16 +22,20 @@ import org.egov.ps.model.PropertyDetails;
 import org.egov.ps.producer.Producer;
 import org.egov.ps.repository.PropertyRepository;
 import org.egov.ps.service.calculation.IEstateRentCollectionService;
+import org.egov.ps.util.PSConstants;
 import org.egov.ps.web.contracts.EstateAccount;
 import org.egov.ps.web.contracts.EstateDemand;
 import org.egov.ps.web.contracts.EstatePayment;
 import org.egov.ps.web.contracts.PaymentStatusEnum;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
@@ -53,10 +64,25 @@ public class EstateDemandGenerationServiceTests {
     	
     	// Arrange
     	setDummydata();
+    	
+    	when(propertyRepository.getProperties(anyObject())).thenReturn(propertyDummyList);
+    	when(propertyRepository.getPropertyDetailsEstateDemandDetails(anyList())).thenReturn(estateDemandDummyList);
+    	when(propertyRepository.getPropertyDetailsEstatePaymentDetails(anyList())).thenReturn(estatePaymentDummyList);
+    	when(propertyRepository.getAccountDetailsForPropertyDetailsIds(anyList())).thenReturn(estateDummyAccount);
     }
+    
+    @Test
+    public void createDemandTest() {
+    	
+    	AtomicInteger dummayResult = estateDemandGenerationService.createDemand(EstateDemandCriteria.builder().build());
+    	assertTrue("Error, can't update more than one record", 1 >= dummayResult.get());
+    }
+    
 
 	private void setDummydata() {
 		
+	
+		/* Set Dummy data for EstateDemand */		
 		EstateDemand estateDemand = EstateDemand.builder().id(UUID.randomUUID().toString())
 				.status(PaymentStatusEnum.PAID)
 				.propertyDetailsId(UUID.randomUUID().toString())
@@ -78,8 +104,32 @@ public class EstateDemandGenerationServiceTests {
 				.remainingRentPenalty(new Double(268))
 				.remainingGST(new Double(0))
 				.remainingGSTPenalty(new Double(0)).build();
-		estateDemandDummyList.add(estateDemand);
 		
+		EstateDemand estateDemand2 = EstateDemand.builder().id(UUID.randomUUID().toString())
+				.status(PaymentStatusEnum.PAID)
+				.propertyDetailsId(UUID.randomUUID().toString())
+				.generationDate(Long.parseLong("1551378600000"))
+				.collectionPrincipal(new Double(0))
+				.remainingPrincipal(new Double(0))
+				.interestSince(Long.parseLong("1553538600000"))
+				.isPrevious(false)
+				.rent(new Double(2678))
+				.penaltyInterest(new Double(268))
+				.gstInterest(new Double(482))
+				.gst(new Double(18))
+				.collectedRent(new Double(2196))
+				.collectedGST(new Double(482))
+				.collectedGSTPenalty(new Double(0))
+				.collectedRentPenalty(new Double(0))
+				.paid(new Double(0))
+				.remainingRent(new Double(482))
+				.remainingRentPenalty(new Double(268))
+				.remainingGST(new Double(482))
+				.remainingGSTPenalty(new Double(0)).build();
+		estateDemandDummyList.add(estateDemand);
+		estateDemandDummyList.add(estateDemand2);
+		
+		/* Set Dummy data for EstatePayment */
 		EstatePayment estatePayment = EstatePayment.builder()
 				.processed(true)
 				.id(UUID.randomUUID().toString())
@@ -91,6 +141,7 @@ public class EstateDemandGenerationServiceTests {
 				.build();
 		estatePaymentDummyList.add(estatePayment);
 		
+		/* Set Dummy data for EstateAccount */
 		estateDummyAccount = EstateAccount.builder()
 				.remainingSince(Long.parseLong("1553538600000"))
 				.id(UUID.randomUUID().toString())
@@ -98,6 +149,7 @@ public class EstateDemandGenerationServiceTests {
 				.remainingAmount(new Double(0))
 				.build();
 		
+		/* Set Dummy data for PaymentConfigItems */
 		PaymentConfigItems paymentConfigItems = new PaymentConfigItems();
 		paymentConfigItems.setGroundRentStartMonth(Long.parseLong("1"));
 		paymentConfigItems.setGroundRentEndMonth(Long.parseLong("20"));
@@ -105,20 +157,34 @@ public class EstateDemandGenerationServiceTests {
 		paymentConfigItems.setId(UUID.randomUUID().toString());
 		paymentConfigItems.setPaymentConfigId(UUID.randomUUID().toString());
 		
+		PaymentConfigItems paymentConfigItems2 = new PaymentConfigItems();
+		paymentConfigItems2.setGroundRentStartMonth(Long.parseLong("21"));
+		paymentConfigItems2.setGroundRentEndMonth(Long.parseLong("40"));
+		paymentConfigItems2.setGroundRentAmount(new BigDecimal(2000));
+		paymentConfigItems2.setId(UUID.randomUUID().toString());
+		paymentConfigItems2.setPaymentConfigId(UUID.randomUUID().toString());
+		
+		/* Set Dummy data for PaymentConfig */
 		PaymentConfig paymentConfig = PaymentConfig.builder()
-				.groundRentBillStartDate(Long.parseLong("1408905000000"))
+				.groundRentBillStartDate(Long.parseLong("1566671400000"))
 				.id(UUID.randomUUID().toString())
 				.propertyDetailsId(UUID.randomUUID().toString())
-				.paymentConfigItems(Arrays.asList(paymentConfigItems))
+				.paymentConfigItems(Arrays.asList(paymentConfigItems,paymentConfigItems2))
+				.isGroundRent(true)
+				.groundRentGenerateDemand("18")
+				.groundRentGenerationType(PSConstants.MONTHLY)
 				.build();
 		
+		/* Set Dummy data for PropertyDetails */
 		PropertyDetails propertyDetails = PropertyDetails.builder()
 				.estateDemands(estateDemandDummyList)
 				.estatePayments(estatePaymentDummyList)
 				.estateAccount(estateDummyAccount)
 				.paymentConfigs(Arrays.asList(paymentConfig))
+				.id(UUID.randomUUID().toString())
 				.build();
 		
+		/* Set Dummy data for EstateDemand */
 		propertyDummyList.add(Property.builder()
 				.id(UUID.randomUUID().toString())
 				.propertyDetails(propertyDetails)
